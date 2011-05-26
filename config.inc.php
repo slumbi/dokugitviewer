98fae36438b4fddc47918e902160852d8344730f
Akos <akos@liligo.com>
2011-05-26 23:42:33 +0200

    configure for my local

--- config.inc.php
+++ config.inc.php
@@ -1,18 +1,20 @@
 <?php
 
 // location of your git executable
-define('GIT_EXEC', '/opt/local/bin/git');
+define('GIT_EXEC', 'D:/install/git/PortableGit-1.7.3.1-preview20101002/bin/git.exe');
 
 // delimeter character, don't change this unless you change functions
 // from git-utils.inc.php
 define('DELIMETER', '|');
 
 // root directory for your repositories
-define('ROOT_DIR', 'WHERE ARE YOUR REPOSITORIES LOCATED, ABSOLUTE PATH');
+//define('ROOT_DIR', 'F:/git_repos/');
+define('ROOT_DIR', 'D:/Liligo/workspaceMain/');
+
 
 // date format
 define('DATE_FORMAT','d.m.Y h:m');
 
 // REMEMBER TO REMOVE THIS LINE AFTER YOU HAVE MADE CONFIGURATIONS
-die('Remember to set configuration from '.__FILE__.' and remove the line which starts with "die"');
+//die('Remember to set configuration from '.__FILE__.' and remove the line which starts with "die"');
 ?>
